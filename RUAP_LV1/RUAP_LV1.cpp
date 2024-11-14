773f70c04b992dda986b899d82b33e8ac908f971
lsobacic <lea.sobacic@gmail.com>
2024-11-14 18:48:24 +0100

    Neki tekst..

--- RUAP_LV1/RUAP_LV1.cpp
+++ RUAP_LV1/RUAP_LV1.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Pozdrav iz aplikacije od Antun Radmana!\n";
+    std::cout << "Pozdrav opet!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
